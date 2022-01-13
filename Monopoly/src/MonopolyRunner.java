a6aa32f98bad1bfb2e9789b71d6d743cd13feb6f
jackson.dalexander <jackson.dalexander@MU-C1-10.mullen.pvt.k12.co.us>
2022-01-13 09:00:27 -0700

    working on runner

--- Monopoly/src/MonopolyRunner.java
+++ Monopoly/src/MonopolyRunner.java
@@ -1,9 +1,14 @@
-
+import java.util.Scanner;
 public class MonopolyRunner
 	{
 
 		public static void main(String[] args)
 			{
+			System.out.println("This is the game of Monopoly, Would you like to play?");
+			System.out.println("(1) yes");
+			System.out.println("(2) no");
+			
+			
 				
 				
 				
